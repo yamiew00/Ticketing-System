e8f4d08363f4f8410d9326c543c7bedb7ef30cbb
yamiew <r05221017@gmail.com>
2023-11-18 13:54:06 +0800

    add model project of ticketing system

---
+++ TicketingSystemModel/TicketSystemMongoContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoGogo.Connection;
+
+namespace TicketingSystemModel
+{
+    public class TicketSystemMongoContext : GoContext<TicketSystemMongoContext>
+    {
+        [MongoDatabase]
+        public class Ticketing { }
+
+        public TicketSystemMongoContext(string connectionString) : base(connectionString)
+        {
+        }
+    }
+}
---
+++ TicketingSystemModel/Ticketing/UserEntity.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoGogo.Connection;
+
+namespace TicketingSystemModel.Ticketing
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    [MongoCollection(fromDatabase: typeof(TicketSystemMongoContext.Ticketing),
+                     collectionName: "User")]
+    public class UserEntity
+    {
+        [BsonId]
+        public string Id { get; set; }
+
+        public Account Account { get; set; }
+
+        public PersonalInfo PersonalInfo { get; set; }
+
+        public PersonalActivity PersonalActivity { get; set; }
+    }
+
+    public class Account
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class PersonalInfo
+    {
+        public string FullName { get; set; }
+    }
+
+    public class PersonalActivity
+    {
+        public DateTime RegistrationTime { get; set; }
+
+        public DateTime? LastLoginTime { get; set; }
+    }
+}
