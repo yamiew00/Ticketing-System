de9e34f133d4bb803a6ff2fccf41bfe8e21efc7a
yamiew <r05221017@gmail.com>
2023-11-18 13:33:06 +0800

    initial webapi project

---
+++ TicketingSystemWebApi/Controllers/RegisterController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketingSystemWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+    }
+}
